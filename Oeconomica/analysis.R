a950795e2c4a3faf824f2f0c3ebbc42eecff64ce
racheltoddalper <rachelalper@uchicago.edu>
2024-04-13 20:33:20 -0500

    Update analysis.R

--- Oeconomica/analysis.R
+++ Oeconomica/analysis.R
@@ -99,9 +99,7 @@ ggplot(data = df, aes(x = date)) +
   labs(y = "Sales (logged)", color = 'Legend') +
   theme_excel_new()
 
-#calculation of damages, this is with reg not reg2 bc 
-#i ran into issues, but reg is more conservative also hopefully we can fix and
-#then just switch to reg2
+#calculation of damages
 df = df %>%
   mutate(difference = exp(predicted_zero) - dominant_sales)
 
